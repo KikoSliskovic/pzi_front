bd3761ad27d1b258ec48e3684f0658315b10ed0e
KikoSliskovic <kristijan.sliskovic@fpmoz.sum.ba>
2025-02-24 19:25:22 +0100

    comit

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,73 @@
-<script setup lang="ts">
-import { ref, onMounted } from "vue";
-import axios from "axios";
-import { useRuntimeConfig } from "nuxt/app";
-
-const lectures = ref([]);
-
-// get runtime config
-const runtimeConfig = useRuntimeConfig();
-
-const fetchData = async () => {
-  try {
-    const [lecturesRes, ] = await Promise.all([
-      axios.get(runtimeConfig.public.apiUrl +"/api/lectures"),
-    ]);
-
-    lectures.value = lecturesRes.data;
-  } catch (error) {
-    console.error("Greška pri dohvaćanju podataka:", error);
-  }
-};
-
-onMounted(fetchData);
+<script setup >
 </script>
 
-
 <template>
-  <Navbar />
   <v-main>
-    <div class="fill-height" style="background-color: lightblue">
-      <v-container>
-        <v-row>
-          <v-col>
-            <nuxt-link to="/profil"> Profil </nuxt-link>
-            <br>
-            <nuxt-link to="/login"> Login </nuxt-link>
-            <br>
-            <nuxt-link to="/skener"> Skener </nuxt-link>
-            <br>
-            <nuxt-link to="/generator"> Generator </nuxt-link>
-            <br>
-            <nuxt-link to="/table"> Tablica </nuxt-link>
-            <br>
-          </v-col>
-        </v-row>
-      </v-container>
+    <div class="fill-height">
+      <v-row>
+        <v-col class="v-col-12 v-col-md-6 pb-0">
+          <div class="right-pane">
+            <!-- Right Side Content -->
+            <LoginForma />
+          </div>
+        </v-col>
+        <!-- Right Panel: Hide on smaller screens -->
+        <v-col class="v-col-12 v-col-md-6"
+            style="display: flex; justify-content: center; align-items: center; height: 100vh; position: relative">
+          <v-img
+              src="assets/Logo2.png"
+              max-height="250"
+              alt="Logo2"
+              style="display: block; margin: auto;"
+          ></v-img>
+          <div class="fill-height">
+            <!-- Left Side Content (buttons) -->
+            <a href="https://aai.sum.ba/" target="_blank">
+              <button class="transparent-button bottom-left">O eduID sustavu</button>
+            </a>
+            <a href="https://aai.sum.ba/" target="_blank">
+              <button class="transparent-button bottom-right">Pomoć</button>
+            </a>
+          </div>
+        </v-col>
+      </v-row>
     </div>
   </v-main>
 </template>
 
 
+
 <style scoped>
 
-h1, h2 {
-  text-align: center;
-  margin-bottom: 10px;
+.left-pane {
+  flex: 1;
+  background-color: white;
 }
 
+.right-pane {
+  flex: 1;
+  height: 100vh;
+  background-image: url('assets/pozadina.png');
+  background-size: cover;
+  background-position: center;
+}
+
+.transparent-button {
+  background: none;
+  border: none;
+  color: #0379FD;
+  font-size: 1rem;
+  cursor: pointer;
+  position: absolute;
+}
+
+.bottom-left {
+  bottom: 25px;
+  left: 1%;
+}
+
+.bottom-right {
+  bottom: 25px;
+  right: 4%;
+}
 </style>
